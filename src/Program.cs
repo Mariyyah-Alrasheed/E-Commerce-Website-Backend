bded5139c8305b24f194192b7978eae9944d7b7e
mariyyah <ma_ria111@icloud.com>
2024-05-28 14:19:11 +0300

    For deploy

--- src/Program.cs
+++ src/Program.cs
@@ -41,7 +41,7 @@ builder.Services.AddControllers().AddNewtonsoftJson(options =>
 
 var _config = builder.Configuration;
 
-var dataSourceBuilder = new NpgsqlDataSourceBuilder(@$"Host={_config["Db:Host"]};Username={_config["Db:Username"]};password={_config["Db:Password"]};Database={_config["Db:Database"]}");
+var dataSourceBuilder = new NpgsqlDataSourceBuilder(@$"Host={_config["Db_Host"]};Username={_config["Db_Username"]};password={_config["Db_Password"]};Database={_config["Db_Database"]}");
 dataSourceBuilder.MapEnum<Role>();
 dataSourceBuilder.MapEnum<Status>();
 
@@ -92,7 +92,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins(builder.Configuration["Cors:Origin"]!)
+                          policy.WithOrigins(builder.Configuration["Cors_Origin"]!)
                           .AllowAnyHeader()
                             .AllowAnyMethod()
                             .SetIsOriginAllowed((host) => true)
@@ -109,9 +109,9 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         ValidateAudience = true,
         ValidateLifetime = true,
         ValidateIssuerSigningKey = true,
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SigningKey"]
+        ValidIssuer = builder.Configuration["Jwt_Issuer"],
+        ValidAudience = builder.Configuration["Jwt_Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt_SigningKey"]
         !))
     };
 });
